321b2e5eee75185d671181953a60bc6c67ea9514
Kovalev Vadim <auravadima@gmail.com>
2022-02-20 22:30:49 +0700

    Вторая таска

--- second/src/components/loading/LoadingSpinner.tsx
+++ second/src/components/loading/LoadingSpinner.tsx
@@ -1,20 +1,4 @@
 import React, { FC, useCallback, useEffect, useRef, useState } from 'react'
-const LOADING_KEYS = [
-  'Loading.First',
-  'Loading.Second',
-  'Loading.Third',
-  'Error.Timeout',
-] as const
-const SUCCESS_KEY = 'Success.LoadingFinished' as const
-
-type TAvaliableKeys = typeof LOADING_KEYS[number] | typeof SUCCESS_KEY
-
-type TLoadingSpinnerProps = {
-  step?: number
-  messages?: Record<TAvaliableKeys, string>
-  children: React.ReactElement
-  laodedTimeout?: number
-}
 
 type TState = { value: TAvaliableKeys; done: boolean }
 
